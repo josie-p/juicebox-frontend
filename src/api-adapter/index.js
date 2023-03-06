c69ffa15d170badb4572da3a054645e1b6eb7e06
Your Name <parker.josie.eden@gmail.com>
2023-03-06 14:31:15 -0600

    started getting everything set up, worked on routes

---
+++ src/api-adapter/index.js
@@ -0,0 +1,29 @@
+const BASE_URL = "https://ap-jp-juicebox.onrender.com/api"
+
+function makeHeaders(token){
+    return{
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token
+    }
+}
+
+const logInAPI = async (username, password) => {
+    try{
+
+       const response = await fetch(`${BASE_URL}/users/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        });
+
+        const result = response.json()
+        return result;
+    }catch(error){
+        console.error(error)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Main } from "./components";
+import { Main, HomePage, LogIn } from "./components";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Main/>}>
+            <Route index element={<HomePage/>}/>
+            <Route path="/login" element={<LogIn/>}/>
+        </Route>
+    )
+);
 
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
 
-root.render(<Main />);
+root.render(<RouterProvider router={router}/>);
